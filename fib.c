e7c669ce177b867b52860d059f9d2d66a36d33c2
Brandon Kim <brandonkim@Brandons-Air-86.lan>
2024-09-23 12:19:10 -0700

    updated variable names and comments

--- fib.c
+++ fib.c
@@ -2,60 +2,62 @@
 #include <stdlib.h>
 #include <string.h>
 
-int rfib(int n) {                     // Recursive fib(). Since
-   if (n == 1) {                      // fibonacci #1 is 0, adjust
-      return 0;                       // the other values to fit
-   } else if (n <= 2) {               // the constraints
+// Recursive fib(); fib(1) == 0
+int rfib(int n) {
+   if (n == 1) {
+      return 0;
+   } else if (n <= 2) {
       return 1;
    }
 
-   return rfib(n - 1) + rfib(n - 2);  // Using recursion, find fib(n)
+   return rfib(n - 1) + rfib(n - 2);
 }                                   
 
-int ifib(int n) {                     // Iterative fib(). Same as
-   if (n == 1) {                      // above, make sure fib(1)
-      return 0;                       // is 0. Then adjust the
-   } else if (n == 2) {               // other values
+// Iterative fib(); fib(1) == 0
+int ifib(int n) {
+   if (n == 1) {
+      return 0;
+   } else if (n == 2) {
       return 1;                       
    }
 
-   int a = 0;                         // Set the initial values for
-   int b = 1;                         // the iterative sequence
+   int previous = 0;
+   int current = 1;
    int result = 0;                    
 
-   for (int i = 3; i <= n; ++i) {     // Loop till the Nth number
-      result = a + b;                 // Update the result and
-      a = b;                          // variables for the next
-      b = result;                     // iteration
+   for (int ix = 3; ix <= n; ++ix) {
+      result = previous + current;
+      previous = current;
+      current = result;
    }                                  
 
-   return result;                     // Return the result.
+   return result;
 }                                     
 
-int main(int argc, char* argv[]) {    // cmd line inputs for main()
-   int n;                             // Create and store the cmd
-   sscanf(argv[1], "%d", &n);         // line integer into a variable
+int main(int argc, char* argv[]) {
+   int n_input;
+   sscanf(argv[1], "%d", &n_input);
 
-   char type = argv[2][0];            // Store the type of method
+   char type = argv[2][0];
 
-   FILE *fptr = fopen(argv[3], "r");  // Open the given file
+   FILE *fptr = fopen(argv[3], "r");
    
-   int file_n;                        // Create and store the integer
-   fscanf(fptr, "%d", &file_n);       // within the file into a
-   fclose(fptr);                      // variable and close the file
+   int file_n;
+   fscanf(fptr, "%d", &file_n);
+   fclose(fptr);
    
-   n += file_n;                       // Add the two numbers to get N
+   n_input += file_n;
 
-   int number = 0;
-   if (type == 'i') {                 // If the type of method is 
-      number = ifib(n);               // iterative, call ifib(),
-   } else if (type == 'r') {          // otherwise call rfib().
-      number = rfib(n);
-   } else {                           // If the type is invalid,
-      number = 0;                     // set number to 0
+   int result = 0;
+   if (type == 'i') {
+      result = ifib(n_input);
+   } else if (type == 'r') {
+      result = rfib(n_input);
+   } else {
+      result = 0;
    }
 
-   printf("%d", number);              // Print the result
+   printf("%d", result);
 
    return 0;
 }
